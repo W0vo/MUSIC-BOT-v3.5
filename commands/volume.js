17aeb407837a44344089396c5056178c48fb7205
W0vo <104596037+W0vo@users.noreply.github.com>
2024-02-25 22:08:09 -0500

    Removed links

--- commands/volume.js
+++ commands/volume.js
@@ -46,9 +46,7 @@ module.exports = {
         const embed = new EmbedBuilder()
           .setColor('#d291fe')
           .setAuthor({
-        name: 'Your Music! Your Rules!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157528025739563088/5657-volume-icon.png?ex=6518ef7b&is=65179dfb&hm=1797c2830537a28b5c6a57564517cc509146d02383a69fb4239d7b5d55aceeed&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        name: 'Your Music! Your Rules!'
     })
           .setDescription(`**Adjusting Volume : ** **${vol}/${maxVol}**`);
 
